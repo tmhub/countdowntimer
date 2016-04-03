cb3b90d1d81f34ef094373167bca161afc8cc47d
Vitaliy Dmytruk <vitaliy@mage2cloud.com>
2016-04-03 18:30:50 +0300

    Improved starttimers script

--- skin/frontend/base/default/js/tm/countdowntimer/starttimers.js
+++ skin/frontend/base/default/js/tm/countdowntimer/starttimers.js
@@ -1,39 +1,14 @@
-function TmCountdownTimers(){
+var tmCountdownTimers = new function (){
   
   this.timerFlip = '.tm-cdt-flip';
   this.timerSimple = '.tm-cdt-simple';
   this.timerNotStarted ='.not-started';
-  this.localeName = 'customer_locale',
+  var _localeName = 'cu_LO'; // customer locale
+  var _self = this;
 
-  this.getSecondsLeft = function(obj){
-    var endDate = jQuery(obj).data('end-datetime-utc');
-    var secLeft = 0;
-    if (endDate) {
-      secLeft = endDate - Math.floor(Date.now() / 1000);
-      secLeft = (secLeft < 0) ? 0 : secLeft;
-    }
-    return secLeft;
-  };
-
-  this.setDaysSecondsSimpleTimer = function(obj, secs){
-    var showDays = jQuery(obj).data('display-days');
-    var secLeft = secs;
-    if (showDays) {
-      var days = Math.floor( secLeft / ( 24 * 60 * 60 ) );
-      if (days > 0) {
-        var text = days.toString() + ' ' + Translator.translate('day(s)');
-        jQuery(obj).append('<span class="days">'+text+'</span>');
-        secLeft -= days * 24 * 60 * 60;
-      }
-    };
-    jQuery(obj).data('seconds-left', secLeft);
-  };
-
-  this.start = function(){
-    var self = this;
-    // translations for flipclock labels
-    if (jQuery.isEmptyObject(FlipClock.Lang[self.localeName])) {
-      FlipClock.Lang[self.localeName] = {
+  var _initTranslations = function (){
+    if (jQuery.isEmptyObject(FlipClock.Lang[_localeName])) {
+      FlipClock.Lang[_localeName] = {
         'years' : Translator.translate('Years'),
         'months' : Translator.translate('Months'),
         'days' : Translator.translate('Days'),
@@ -42,34 +17,61 @@ function TmCountdownTimers(){
         'seconds' : Translator.translate('Seconds')
       }
     };
-    // start flip timer
-    jQuery(self.timerNotStarted).filter(self.timerFlip).each(
-      function(index, obj){
-        jQuery(obj).FlipClock(
-          self.getSecondsLeft(obj),
-          {
-            clockFace: jQuery(obj).data('flipface'),
-            countdown: true,
-            language: self.localeName
-          }
-        );
-      }
-    ).removeClass(self.timerNotStarted.substring(1));
-    // start simple timer
-    jQuery(self.timerNotStarted).filter(self.timerSimple).html('').each(
-      function(index, obj){
-        self.setDaysSecondsSimpleTimer(obj, self.getSecondsLeft(obj));
-      }
-    ).startTimer().removeClass(self.timerNotStarted.substring(1));
+  }
+
+  var _getSecondsLeft = function(obj){
+    var end = jQuery(obj).data('end-datetime-utc');
+    var left = 0;
+    if (end) {
+      left = end - Math.floor(Date.now() / 1000);
+      left = (left < 0) ? 0 : left;
+    }
+    return left;
+  };
+
+  var _startFlipClock = function(obj){
+    obj.each(function(i, el){
+      jQuery(el).FlipClock(
+        _getSecondsLeft(el),
+        {
+          clockFace: jQuery(el).data('flipface'),
+          countdown: true,
+          language: _localeName
+        }
+      );
+    });
+  }
+
+  var _startSimpleTimer = function(obj){
+    // prepare data for start
+    obj.html('').each(function(i, el){
+      var secLeft = _getSecondsLeft(el);
+      if (jQuery(el).data('display-days')) {
+        var days = Math.floor( secLeft / ( 24 * 60 * 60 ) );
+        if (days > 0) {
+          secLeft -= days * 24 * 60 * 60;
+          var text = days.toString() + ' ' + Translator.translate('day(s)');
+          jQuery(el).append('<span class="days">'+text+'</span>');
+        }
+      };
+      jQuery(el).data('seconds-left', secLeft);
+    });
+    obj.startTimer();
+  }
+
+  this.start = function(){
+    _initTranslations();
+    var timers = jQuery(_self.timerNotStarted);
+    _startFlipClock(timers.filter(_self.timerFlip));
+    _startSimpleTimer(timers.filter(_self.timerSimple));
+    timers.removeClass(_self.timerNotStarted.substring(1));
   }
 
 }
 
-// global variable
-var tmCountdownTimers;
 // trigger timer start on document ready event
 document.observe("dom:loaded", function() {
-  tmCountdownTimers = new TmCountdownTimers();
+  // tmCountdownTimers = new TmCountdownTimers();
   tmCountdownTimers.start();
   // INITIALIZE EVENTS LISTEN
   // jquery event
