d6d8f7b195594e6f06831ab05f3ef3b447ace69d
Vitaliy Dmytruk <vitaliy@mage2cloud.com>
2016-08-14 23:03:16 +0300

    starttimers.js refactored once again

--- skin/frontend/base/default/js/tm/countdowntimer/starttimers.js
+++ skin/frontend/base/default/js/tm/countdowntimer/starttimers.js
@@ -1,103 +1,119 @@
-var tmCountdownTimers = new function (){
-  
-  this.timerFlip = '.tm-cdt-flip';
-  this.timerSimple = '.tm-cdt-simple';
-  this.timerNotStarted ='.not-started';
-  var _localeName = 'cu_LO'; // customer locale
-  var _self = this;
+;(function($){
 
-  var _initTranslations = function (){
-    if (!(_localeName in FlipClock.Lang)) {
-      FlipClock.Lang[_localeName] = {
-        'years' : Translator.translate('Years'),
-        'months' : Translator.translate('Months'),
-        'days' : Translator.translate('Days'),
-        'hours' : Translator.translate('Hours'),
-        'minutes' : Translator.translate('Minutes'),
-        'seconds' : Translator.translate('Seconds')
-      };
+    if (!$) { return; }
+
+    function _getSecondsLeft(obj){
+        var end = $(obj).data('endDatetimeUtc');
+        var left = 0;
+        if (end) {
+          left = end - Math.floor(Date.now() / 1000);
+        }
+        return (left < 0) ? 0 : left;
     };
-  }
 
-  var _initFonts = function (){
-    WebFontConfig = {
-      google: { families: [ 'Fredoka+One::latin', 'Josefin+Sans::latin' ] }
+    function _startFlipClock(timers){
+        timers.each(function(i, el){
+            $(el).FlipClock(
+                _getSecondsLeft(el),
+                {
+                    clockFace: $(el).data('flipface'),
+                    countdown: true,
+                    language: _localeName
+                }
+            );
+        });
     };
-    (function() {
-      var wf = document.createElement('script');
-      wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-      wf.type = 'text/javascript';
-      wf.async = 'true';
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(wf, s);
-    })();
-  }
 
-  var _getSecondsLeft = function(obj){
-    var end = jQuery(obj).data('end-datetime-utc');
-    var left = 0;
-    if (end) {
-      left = end - Math.floor(Date.now() / 1000);
-      left = (left < 0) ? 0 : left;
+    function _startSimpleTimer(timers){
+        // prepare data for start
+        timers.html('').each(function(i, el){
+            var secLeft = _getSecondsLeft(el);
+            if ($(el).data('displayDays')) {
+                var days = Math.floor( secLeft / ( 24 * 60 * 60 ) );
+                if (days > 0) {
+                    secLeft -= days * 24 * 60 * 60;
+                    var text = days.toString() + ' '
+                        + Translator.translate('day(s)');
+                    $(el).append('<span class="days">'+text+'</span>');
+                }
+            };
+            $(el).data('secondsLeft', secLeft);
+        });
+        timers.startTimer();
     }
-    return left;
-  };
 
-  var _startFlipClock = function(obj){
-    obj.each(function(i, el){
-      jQuery(el).FlipClock(
-        _getSecondsLeft(el),
-        {
-          clockFace: jQuery(el).data('flipface'),
-          countdown: true,
-          language: _localeName
+    var _timerFlip = '.tm-cdt-flip',
+        _timerSimple = '.tm-cdt-simple',
+        _timerNotStarted ='.not-started',
+        _localeName = 'cu_LO',
+        _texts = {
+            'years' : 'Years',
+            'months' : 'Months',
+            'days' : 'Days',
+            'hours' : 'Hours',
+            'minutes' : 'Minutes',
+            'seconds' : 'Seconds'
+        };
+
+    var CountdownTimers = function(){};
+
+    CountdownTimers.prototype = {
+
+        initialize: function() {
+            // initialize translation
+            if (!(_localeName in FlipClock.Lang)) {
+                var transl = {};
+                for (key in _texts) {
+                    transl[key] = Translator.translate(_texts[key]);
+                }
+                FlipClock.Lang[_localeName] = transl;
+            };
+            // initialize additional fonts
+            WebFontConfig = {
+                google: { families: [ 'Fredoka+One::latin', 'Josefin+Sans::latin' ] }
+            };
+            (function() {
+                var wf = document.createElement('script');
+                wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+                wf.type = 'text/javascript';
+                wf.async = 'true';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(wf, s);
+            })();
+        },
+
+        start: function(){
+            var timers = $(_timerNotStarted);
+            _startFlipClock(timers.filter(_timerFlip));
+            _startSimpleTimer(timers.filter(_timerSimple));
+            timers.removeClass(_timerNotStarted.substring(1));
         }
-      );
-    });
-  }
 
-  var _startSimpleTimer = function(obj){
-    // prepare data for start
-    obj.html('').each(function(i, el){
-      var secLeft = _getSecondsLeft(el);
-      if (jQuery(el).data('display-days')) {
-        var days = Math.floor( secLeft / ( 24 * 60 * 60 ) );
-        if (days > 0) {
-          secLeft -= days * 24 * 60 * 60;
-          var text = days.toString() + ' ' + Translator.translate('day(s)');
-          jQuery(el).append('<span class="days">'+text+'</span>');
-        }
-      };
-      jQuery(el).data('seconds-left', secLeft);
-    });
-    obj.startTimer();
-  }
+    };
 
-  this.start = function(){
-    _initTranslations();
-    _initFonts();
-    var timers = jQuery(_self.timerNotStarted);
-    _startFlipClock(timers.filter(_self.timerFlip));
-    _startSimpleTimer(timers.filter(_self.timerSimple));
-    timers.removeClass(_self.timerNotStarted.substring(1));
-  }
+    window.tmCountdownTimers = new CountdownTimers();
 
-}
+})((typeof jQuery != 'undefined')?jQuery:false);
 
 // trigger timer start on document ready event
 document.observe("dom:loaded", function() {
-  tmCountdownTimers.start();
-  // INITIALIZE EVENTS LISTEN
-  // jQuery event
-  jQuery(document).on("tm:countdowntimer:start", function(){tmCountdownTimers.start()});
-  // prototype event
-  var eventsArr = [
-    "tm:countdowntimer:start",
-    "quickshopping:previewloaded",
-    "ajaxlayerednavigation:ready",
-    "AjaxPro:onComplete:after"
-  ];
-  eventsArr.map(function(eventName){
-    document.observe(eventName, function(){tmCountdownTimers.start()});
-  });
+    if (typeof jQuery === 'undefined') {
+        console.warn('Countdown Timer can not find jQuery');
+        return;
+    }
+    tmCountdownTimers.initialize();
+    tmCountdownTimers.start();
+    // INITIALIZE EVENTS LISTEN
+    // jQuery event
+    jQuery(document).on("tm:countdowntimer:start", tmCountdownTimers.start);
+    // prototype event
+    var eventsArr = [
+        "tm:countdowntimer:start",
+        "quickshopping:previewloaded",
+        "ajaxlayerednavigation:ready",
+        "AjaxPro:onComplete:after"
+    ];
+    eventsArr.map(function(eventName){
+        document.observe(eventName, tmCountdownTimers.start);
+    });
 });
